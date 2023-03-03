1d08b1400469a072d29cfc8904f4e30083067195
GrandQughtere <94853120+GrandQughtere@users.noreply.github.com>
2023-03-04 03:30:58 +0800

    Add files via upload

---
+++ instruction.tcl
@@ -0,0 +1,3 @@
+vsim -i -classdebug -solvefaildebug -coverage -coverstore D:/cache/questa -testname write_test -sv_seed 0 +TESTNAME=write_test -l write_test.log cache.tb
+
+vsim -i -classdebug -solvefaildebug -coverage -coverstore D:/cache/questa -testname replace_algorithm_test -sv_seed 0 +TESTNAME=replace_algorithm_test -l replace_algorithm_test.log cache.tb
